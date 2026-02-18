536f2db42f0e233329d6d2a135f6d5a205d5af00
GenesisNUP30 <jdirproy@gmail.com>
2026-02-18 23:29:15 +0100

    Creada funcion login para verificar username y pssword. Username añadido a tabla users

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('username', $request->username)->first();
+        
+    }
 }
