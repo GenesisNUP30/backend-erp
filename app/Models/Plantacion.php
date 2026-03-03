6478a4fcccbdccdb8968e9eaff3d8001de15169f
GenesisNUP30 <jdirproy@gmail.com>
2026-03-03 11:40:36 +0100

    SoftDeletes incluido en modelos y tablas

--- app/Models/Plantacion.php
+++ app/Models/Plantacion.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Plantacion extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'parcela_id',
